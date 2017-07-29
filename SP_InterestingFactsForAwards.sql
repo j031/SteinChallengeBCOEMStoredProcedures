c0b42a67875713b1e503c6ad9e8319c292c5411b
Joel Taylor <joel@taylor-consulting.com>
2017-07-28 20:21:13 -0500

    Add more interesting facts. This time how many entrants per club and entries per club.

--- SP_InterestingFactsForAwards.sql
+++ SP_InterestingFactsForAwards.sql
@@ -13,7 +13,9 @@ BEGIN
 		 2. # of entries by state
 		 3. Avg Number of entries per entrant
          4. Max Entries per entrant
-         5. Avg Number of entries per state
+         5. Avg Number of entries per entrant per state
+         6. Entries by Club
+         7. Entrants per club
 	 
 	 =============================================
 	 */
@@ -27,6 +29,7 @@ BEGIN
 		, EntryId int
 		, Received bit
 		, State varchar(2)
+        , Club varchar(100)
 		, PRIMARY KEY (Id)
 	 );
 	 
@@ -38,23 +41,25 @@ BEGIN
 		Id int NOT NULL AUTO_INCREMENT
 		, EntrantId int
 		, State varchar(2)
+        , Club varchar(100)
 		, PRIMARY KEY (Id)
 	 );
 	 
 	 CREATE  INDEX brewerid ON tmpEntrants(EntrantId);  
 	 
 	 
-	 INSERT INTO tmpEntries (EntrantId, EntryId, Received, State)
+	 INSERT INTO tmpEntries (EntrantId, EntryId, Received, State, Club)
 	 /* values */
-	   (  SELECT brewer.id, beer.id, brewReceived, brewer.brewerState
+	   (  SELECT brewer.id, beer.id, brewReceived, brewer.brewerState, brewer.brewerClubs
 		  FROM bcoem_brewer as brewer
 		  INNER JOIN bcoem_brewing as beer on brewer.id = beer.brewBrewerId
 		);
 	  
-	 INSERT INTO tmpEntrants (EntrantId, State) 
+	 INSERT INTO tmpEntrants (EntrantId, State, Club) 
 	 /* values */ 
-		( SELECT distinct e.EntrantId, e.State
+		( SELECT distinct e.EntrantId, e.State, b.brewerClubs
 		  FROM tmpEntries e
+          INNER JOIN bcoem_brewer as b on e.EntrantId = b.Id
 		  Where e.Received=1
 		);
 	  
@@ -130,5 +135,18 @@ BEGIN
 	  Order By AvgEntriesPerParticipant desc;
       
       
+      
+      /* By Club */
+      
+      SELECT count(p.Club) as ClubEntrant, p.Club FROM 
+      tmpEntrants p
+      Group by p.Club
+      order by p.Club;
+      
+      SELECT count(e.Club) as ClubEntry, e.Club FROM 
+      tmpEntries e 
+      where e.Received = 1
+      Group by e.Club
+      Order by e.Club;
 END$$
 DELIMITER ;
